fd78e634bf053eccb84a40502d2a3e7fe1b8decd
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2025-08-15 14:40:41 +0200

    update

--- src/plugin.js
+++ src/plugin.js
@@ -1,4 +1,4 @@
-// noinspection JSUnusedGlobalSymbols
+// noinspection JSUnusedGlobalSymbols, JSUnresolvedReference
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 import * as fs from 'fs';
